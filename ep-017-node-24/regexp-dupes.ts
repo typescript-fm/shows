91b5739d228461f83fe35555afd4544ac70366fa
Kamran Ayub <kamran@hey.com>
2025-05-14 16:00:06 -0500

    explain regexp dupe cap groups

--- ep-017-node-24/regexp-dupes.ts
+++ ep-017-node-24/regexp-dupes.ts
@@ -1,1 +1,6 @@
-console.log("2025-05-14".match(/(?<year>[0-9]{4})-[0-9]{2}|[0-9]{2}-(?<year>[0-9]{4})/))
+// The same group name "year" appears twice, allowing extraction of a year value 
+// regardless of date format (yyyy-mm or mm-yyyy)
+const yearMatcher = /(?<year>[0-9]{4})-[0-9]{2}|[0-9]{2}-(?<year>[0-9]{4})/;
+
+console.log("2025-05", "2025-05".match(yearMatcher).groups?.year)
+console.log("05-2025", "05-2025".match(yearMatcher).groups?.year)
